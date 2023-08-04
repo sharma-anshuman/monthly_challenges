a4125274823aadd1d2e23a6ca4e1672fd34c5547
Anshuman Sharma <code4anshu@gmail.com>
2023-08-04 18:42:52 +0530

    practiced templates

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.show_months, name="challenge_path"),
     path("<int:month>", views.handle_monthly_num_challenge),
     path("<str:month>", views.handle_monthly_challenge, name='month-challenge')
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,8 +1,7 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
-
-# Create your views here.
+from django.template.loader import render_to_string
 
 goals = {
     "january": "Wake up at 5 every morning",
@@ -20,6 +19,15 @@ goals = {
 }
 
 
+def show_months(request):
+    months = list(goals.keys())
+    redirect_path = reverse("challenge_path")
+    return render(request, "challenges/index.html", {
+        "months": months,
+        "path": redirect_path
+    })
+
+
 def handle_monthly_num_challenge(request, month):
     if 0 < month <= 12:
         curr_month = list(goals.keys())[month-1]
@@ -32,6 +40,9 @@ def handle_monthly_num_challenge(request, month):
 def handle_monthly_challenge(request, month):
     try:
         habit = goals[month]
-        return HttpResponse(habit)
+        return render(request, "challenges/challenge.html", {
+            "month": month,
+            "habit": habit
+        })
     except HttpResponseNotFound:
         return HttpResponseNotFound("This month is not supported")
